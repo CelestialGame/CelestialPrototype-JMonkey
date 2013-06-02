9600df544eac68ebd36a916da032f1487fb34053
kthorne <kevint@aptitech.net>
2013-06-02 00:03:51 -0600

    Trees!

--- Celestial/src/com/celestial/WorldCreation/PlanetGenerator.java
+++ Celestial/src/com/celestial/WorldCreation/PlanetGenerator.java
@@ -38,9 +38,12 @@ public class PlanetGenerator {
             for(int j=0; j<16; j++)
             {
                 makeChunk(locx+(i*16), locy, locz+(j*16), Side);
+                int x1 = (int) locx+(i*16);
+                int y1 = (int) locy;
+                int z1 = (int) locz+(j*16);
+                makeTreeAt(new Vector3Int(x1, y1, z1), Side);
             }
         }
-        
         sideNode.addControl(Side);
         return Side;
     }
@@ -78,6 +81,44 @@ public class PlanetGenerator {
         }
     }
     
+    public void makeTreeAt(Vector3Int loc, BlockTerrainControl blockTerrain) {
+    	//int y = getTopBlock(loc,blockTerrain);
+    	int y = loc.getY() +16;
+    	int x = 0;
+    	int z = 0;
+    	Random randomGenerator = new Random();
+        for (int idx = 1; idx <= 10; ++idx){
+            int rInt = randomGenerator.nextInt(10);
+            if(rInt == 2 || rInt == 5) {
+            	x = loc.getX() + 13;
+            	z = loc.getZ() + 16;
+            } else if (rInt == 3 || rInt == 6) {
+            	x = loc.getX() + 5;
+            	z = loc.getZ() + 7;
+            } else if (rInt == 4 || rInt == 7) {
+            	x = loc.getX() + 8;
+            	z = loc.getZ() + 2;
+            } else {
+            	x = loc.getX() + 10;
+            	z = loc.getZ() + 10;
+            }
+        }
+    	
+    	blockTerrain.setBlock(new Vector3Int(x, y, z), Block_Wood.class);
+    	blockTerrain.setBlock(new Vector3Int(x, y+1, z), Block_Wood.class);
+    	blockTerrain.setBlock(new Vector3Int(x, y+1, z), Block_Wood.class);
+    	blockTerrain.setBlock(new Vector3Int(x, y+2, z), Block_Wood.class);
+    	blockTerrain.setBlock(new Vector3Int(x, y+3, z), Block_Wood.class);
+    	blockTerrain.setBlock(new Vector3Int(x+1, y+3, z), Block_Leaves.class);
+    	blockTerrain.setBlock(new Vector3Int(x, y+3, z+1), Block_Leaves.class);
+    	blockTerrain.setBlock(new Vector3Int(x+1, y+3, z+1), Block_Leaves.class);
+    	blockTerrain.setBlock(new Vector3Int(x-1, y+3, z), Block_Leaves.class);
+    	blockTerrain.setBlock(new Vector3Int(x, y+3, z-1), Block_Leaves.class);
+    	blockTerrain.setBlock(new Vector3Int(x-1, y+3, z-1), Block_Leaves.class);
+    	blockTerrain.setBlock(new Vector3Int(x, y+4, z), Block_Leaves.class);
+    	
+    }
+    
     public void makeCubeAt(double dx, double dy, double dz, int BlockType, BlockTerrainControl blockTerrain) {
         //make ground
          //To set a block, just specify the location and the block object
@@ -96,6 +137,22 @@ public class PlanetGenerator {
             blockTerrain.setBlock(new Vector3Int(x, y, z), Block_Dirt.class);
         }
 
+    }
+    
+
+	private int getTopBlock(Vector3Int location, BlockTerrainControl blockTerrain2) {
+	 
+        int x = +location.getX() / blockTerrain2.getSettings().getChunkSizeX();
+        int y = +location.getY() / blockTerrain2.getSettings().getChunkSizeY();
+        int z = +location.getZ() / blockTerrain2.getSettings().getChunkSizeZ();
+ 
+        while (blockTerrain2.getChunk(new Vector3Int(x, y, z)).isBlockOnSurface(location) == false) {
+            location.add(0, 1, 0);
+        }
+ 
+        int topBlock;
+        topBlock = location.getY();
+        return topBlock;
     }
     
 }
